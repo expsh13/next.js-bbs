6afb4d0ce26fb6385516865d170b64fe9087eee6
Satoki Hata <satokihata@gmail.com>
2024-10-12 17:12:19 +0900

    migration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { BBSCardList } from "./components/BBSCard/BBSCardList";
+import prisma from "@/lib/prismaClient";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("http://localhost:3000/api/post", {
+    cache: "no-store",
+  });
+
   return (
     <main className="">
       <BBSCardList />
