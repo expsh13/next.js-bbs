62173a90d3a467a69fdeab6ab1fed6b72be49d2e
Satoki Hata <satokihata@gmail.com>
2024-10-14 23:01:21 +0900

    once

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { BBSCardList } from "./components/BBSCard/BBSCardList";
-import prisma from "@/lib/prismaClient";
 
-export default async function Home() {
+async function getBBSAll(params: type) {
   const response = await fetch("http://localhost:3000/api/post", {
     cache: "no-store",
   });
 
+  const data = await response.json();
+  console.log(data);
+
+  return data;
+}
+
+export default async function Home() {
   return (
     <main className="">
       <BBSCardList />
